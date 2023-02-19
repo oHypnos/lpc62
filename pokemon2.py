6a9dfbc4cfb96021b246caa970c489d74c6f7989
FJMarin <124079862+oHypnos@users.noreply.github.com>
2023-02-19 15:07:35 -0600

    Update pokemon2.py

--- pokemon2.py
+++ pokemon2.py
@@ -18,3 +18,8 @@ def get_pokmeons (url="https://pokeapi.co/api/v2/pokemon-form/")
 if __name__=="__main__":
 	url = "https://pokeapi.co/api/v2/pokemon-form/"
 	get_pokemons()
+#Script en python que consulta el api de pokemon
+#Para listar los nombres de pokemon pero se le agrego
+#Interaccion para que listaras mas pokemon segun se vaya requiriendo.
+#Contribuyu: Fernando Javier Marin Salas
+#Fecha: 19/02/2023
